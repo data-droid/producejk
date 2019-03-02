d4b3da26ce17d6f8d146eb0be5dd6e4eef8da39e
data-droid <blue44rain@gmail.com>
2019-03-02 16:01:42 +0900

    Initial commit

---
+++ src/main/java/jaekyung/dev/producejk/ProducejkApplication.java
@@ -0,0 +1,13 @@
+package jaekyung.dev.producejk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducejkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducejkApplication.class, args);
+    }
+
+}
